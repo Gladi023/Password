affa7033cf4bc5d5d2131a695e4b4a37874bf1dc
Gladi023 <Gladimar.Villamil@gmail.com>
2022-10-10 12:39:10 -0700

    generate button event listeners

--- js/index.js
+++ js/index.js
@@ -1,63 +1,29 @@
 
-// Assignment code here
-var generateBtn = document.querySelector("generate");
-var Ualphabet =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var special = ["*", "&", "%", "#", "!"]
-var length
-var symbols
-var numbers1
-var lowerletters
-var upperletters
-var arrayinclude = []
-var generatedPassword = ""
 
-function criteria () {
-    length = prompt("length of password?")
-    
-    if (length < 8) {
-        alert("Password must be longer than 8 characters");
-        criteria();
-    } else if (length > 50) {
-        alert("Password must be les than 50 characters");
-        criteria();
-    }
-    generatedPassword = ""
-    return length
-}
-//letters
-function criteriaLower() {
-    lowerletters = confirm("include lower letter?");
-    return lowerletters;
-}
-//capital letters
-function criteriaUpper() {
-    upperletters = confirm("include capital letters?");
-    return upperletters;
-}
-//numbers
-function criterianumbers() {
-    numbers1 = confirm("include numbers?");
-    return numbers1;
-}
-//symbols
-function criteriaSymbols() {
-    symbols = confirm("include symbols?");
-    return symbols;
-}
+ // Assignment Code
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+ var generateBtn = document.querySelector("generate");
+ const Ualphabet =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+ const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+ const special = ["*", "&", "%", "#", "!"]
+ var length
+ var symbols
+ var number
+ var lowerletters
+ var upperletters
+ var arrayinclude = []
+ var generatedPassword = ""
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//const specialCharacters = "!@#$%^&*()";
+const generateButton = document.getElementById('generate')
+function generatePassword() {
+    var passwordLength = prompt("Please enter number of characters between 8 and 128.")
+    generatePassword();
+//generateButton.addEventListener('click', writePassword);
+let lowercase = document.getElementById("lowerletters");
+let special = document.getElementById("special");
+let uppercase = document.getElementById("upperletters");
+let numbers = document.getElementById("number");
